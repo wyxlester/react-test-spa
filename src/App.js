7100117c042dd9979d8ecee4e7f2019e39d6b653
lester wee <lester.wee.68@gmail.com>
2023-01-14 05:03:17 +0800

    I finally figured out on my own and ocmpleted my first React SPA wihout styling. Yayyyyyyyyy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import Nav from './components/Nav'
+import Home from './Home'
+import Stuff from './Stuff'
+import Contact from './Contact'
+import {
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Nav />
-      <div className="container pt-3 pb-5">
-        <h1>Hello World</h1>
+      <h1>Simple SPA</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/stuff">Stuff</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/stuff" element={<Stuff />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
       </div>
     </div>
   );
